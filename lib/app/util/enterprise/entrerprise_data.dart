80bffe920be53e0cbe4eec49aa19d6b45055de55
Vinicius_Cortez <cortezvinicius11@gmail.com>
2021-09-12 15:11:33 -0300

    update util enterprise

---
+++ lib/app/util/enterprise/entrerprise_data.dart
@@ -0,0 +1,9 @@
+import 'package:projeto_chat_suporte/app/modules/model/service_contrato.dart';
+
+class EntrerpresiData{
+
+  static final String name = "Nitia";
+  static final String id = "1";
+  static final List<Contrato> contrato = [];
+
+}
