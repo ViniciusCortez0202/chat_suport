5103a6809d8198eb9e16cef422f2481f1cd92240
Vinicius_Cortez <cortezvinicius11@gmail.com>
2021-10-19 16:56:39 +0000

    refactored model_service class

--- lib/app/modules/model/service_contrato.dart
+++
@@ -1,8 +0,0 @@
-class Contrato{
-  int idService;
-  String serviceName;
-  bool isContrato;
-  String dataContrato;
-
-  Contrato({required this.idService, required this.isContrato, required this.dataContrato, required this.serviceName});
-}
--- lib/app/modules/model/services_mode.dart
+++ lib/app/modules/model/services_mode.dart
@@ -1,8 +1,38 @@
-class ServicesModel{
+class ServicesModel {
   String idService;
   String serviceName;
   String description;
   String cost;
+  bool isHire;
+  String dateHire;
 
-  ServicesModel({required this.idService, required this.serviceName, required this.cost, required this.description});
+  ServicesModel({
+    required this.idService,
+    required this.serviceName,
+    required this.cost,
+    required this.description,
+    required this.isHire,
+    required this.dateHire,
+  });
+
+  factory ServicesModel.fromJson(json) {
+    return ServicesModel(
+        idService: json['idjob'],
+        serviceName: json['name'],
+        description: json['description'],
+        cost: json['cost'],
+        isHire: json['isHire'],
+        dateHire: json['dateHire']);
+  }
+
+  Map toJson(){
+    return {
+      'idService': idService,
+      'serviceName': serviceName,
+      'description': description,
+      'cost': cost,
+      'isHire': isHire,
+      'dateHire': dateHire,
+    };
+  }
 }
