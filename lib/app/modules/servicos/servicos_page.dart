ff710e2ea201d798d64c13408b23cb1edf81015b
Vinicius_Cortez <cortezvinicius11@gmail.com>
2021-10-19 19:15:17 +0000

    get all jobs and show at screen

--- lib/app/modules/servicos/servicos_page.dart
+++ lib/app/modules/servicos/servicos_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
 import 'package:projeto_chat_suporte/app/modules/appBar/appBar_page.dart';
-import 'package:projeto_chat_suporte/app/modules/model/service_contrato.dart';
+import 'package:projeto_chat_suporte/app/modules/model/services_mode.dart';
 import 'package:projeto_chat_suporte/app/modules/servicos/servicos_store.dart';
 import 'package:flutter/material.dart';
 
@@ -16,44 +18,71 @@ class ServicosPageState extends State<ServicosPage> {
 
   @override
   Widget build(BuildContext context) {
-    List<Contrato> list = controller.contrato;
+    controller.jobs(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Modular.get<AppBarPage>(),
-      ),     
-      body: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      child: ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (_, index) {
-          return Container(
-            height: 75,
-            margin: EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(30))),
-            alignment: Alignment.center,
-            child: ListTile(
-              onTap: (){
-                Modular.to.pushNamed('/servicos/descriptionService', arguments: Contrato(idService: index, serviceName: list[index].serviceName, dataContrato: "", isContrato: list[index].isContrato));
-              },
-              title: Text(
-                list[index].serviceName,
-                style: TextStyle(fontSize: 18, color: Colors.blueGrey),
-              ),
-              trailing: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  list[index].isContrato ? Icon(Icons.check, color: Colors.green, size: 30) : Icon(Icons.close, color: Colors.red, size: 30,),
-                  list[index].isContrato ? Text(list[index].dataContrato, style: TextStyle(fontSize: 10,)) : SizedBox.shrink()
-                ],                
-              ),
-            ),
-          );
-        },
-      ),
-    ),
-    );
+        appBar: AppBar(
+          title: Modular.get<AppBarPage>(),
+        ),
+        body: Observer(builder: (_) {
+          List<ServicesModel> list = controller.services;
+          return controller.servicesFuture.status == FutureStatus.pending
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: ListView.builder(
+                    itemCount: list.length,
+                    itemBuilder: (_, index) {
+                      return Container(
+                        height: 75,
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                        alignment: Alignment.center,
+                        child: ListTile(
+                          onTap: () {
+                            Modular.to.pushNamed('/servicos/descriptionService',
+                                arguments: ServicesModel(
+                                    idService: index.toString(),
+                                    serviceName: list[index].serviceName,
+                                    dateHire: "",
+                                    isHire: list[index].isHire,
+                                    cost: "",
+                                    description: ""));
+                          },
+                          title: Text(
+                            list[index].serviceName,
+                            style:
+                                TextStyle(fontSize: 18, color: Colors.blueGrey),
+                          ),
+                          trailing: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              list[index].isHire
+                                  ? Icon(Icons.check,
+                                      color: Colors.green, size: 30)
+                                  : Icon(
+                                      Icons.close,
+                                      color: Colors.red,
+                                      size: 30,
+                                    ),
+                              list[index].isHire
+                                  ? Text(list[index].dateHire,
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                      ))
+                                  : SizedBox.shrink()
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                );
+        }));
   }
 }
--- lib/app/modules/servicos/servicos_store.dart
+++ lib/app/modules/servicos/servicos_store.dart
@@ -1,15 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
-import 'package:projeto_chat_suporte/app/modules/model/service_contrato.dart';
+import 'package:projeto_chat_suporte/app/modules/model/services_mode.dart';
+import 'package:projeto_chat_suporte/app/modules/servicos/repositories/jobs_repository.dart';
+import 'package:projeto_chat_suporte/app/util/enterprise/entrerprise_data.dart';
+import 'package:projeto_chat_suporte/app/util/indicators/snack_bar.dart';
 
 part 'servicos_store.g.dart';
 
 class ServicosStore = _ServicosStoreBase with _$ServicosStore;
 abstract class _ServicosStoreBase with Store {
-  
 
-  List<Contrato> contrato = [
-    Contrato(idService: 1, serviceName: "ARPA", isContrato: true, dataContrato: "28/07/2022"),
-    Contrato(idService: 1, serviceName: "INFRAESTRUTURA", isContrato: false, dataContrato: ""),
-    Contrato(idService: 1, serviceName: "BACKUP", isContrato: false, dataContrato: ""),
-  ];
+  @observable
+  ObservableFuture<List<ServicesModel>> servicesFuture = ObservableFuture.value([]);
+
+  @observable
+  List<ServicesModel> services = [];
+
+
+  @action
+  jobs(context) async{
+    JobsRespository respository = JobsRespository(Modular.get<Dio>());
+
+    try {
+      servicesFuture = ObservableFuture(respository.getAllJobs(EntrerpresiData.id, ""));
+      services  = await servicesFuture;
+    } catch (e) {
+      print(e);
+      showSnackbarMessage(context, "Não foi possível obter os serviços, tente novamente mais tarde...", Colors.black);
+    }
+  }
+
 }
